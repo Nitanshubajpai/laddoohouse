1fa5e86bd9ec7b48c96545c2cce5674a67cc11e6
Nitanshubajpai <nitanshubajpai@gmail.com>
2026-04-12 13:20:29 +0530

    custom order and web app

--- laddoohouse/urls.py
+++ laddoohouse/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('', include('store.urls')),
     path('dashboard/', include('dashboard.urls')),
     re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+    re_path(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
 ]
